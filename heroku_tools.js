7d8ded286d88891b16854b3c624300dee6a04905
Terry Chong <tc216997@gmail.com>
2017-06-22 06:16:47 +0800

    first commit

---
+++ heroku_tools.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const exec = require('child_process').exec,
+      fs = require('fs'),
+      path = require('path');
+let cmd = 'heroku config:set ';
+
+fs.readFile(path.resolve(__dirname, '.env'), 'utf8', (err, data) => {
+  let array = data.split('\r\n');
+  let i = 0;
+  array.map( item => {
+    exec(cmd + item, (err, stdout, stderr) => {
+      if(err) console.log(err);
+      console.log(stdout)
+    });
+  });
+});
+
